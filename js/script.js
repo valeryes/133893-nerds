47e33c577e812c26d720ceda7fe7f0b581046f3a
Ilia Sachev <redly.makeup@gmail.com>
2016-02-22 20:21:53 +0400

    Правки по базовым критериям после консультации

--- js/script.js
+++ js/script.js
@@ -2,19 +2,22 @@ var btnOpen = document.querySelector(".btn-mail");
 var popup = document.querySelector(".modal-content");
 var close = document.querySelector(".modal-content-close");
 var login = popup.querySelector("[name=login]");
+
 btnOpen.addEventListener("click", function(event) {
-event.preventDefault();
-popup.classList.add("modal-content-show");
-login.focus();
+  event.preventDefault();
+  popup.classList.add("modal-content-show");
+  login.focus();
 });
-close.addEventListener("click", function(event){
-event.preventDefault();
-popup.classList.remove("modal-content-show");
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("modal-content-show");
 });
-window.addEventListener("keydown", function(event){
-if (event.keyCode == 27){
-  if (popup.classList.contains("modal-content-show")){
-popup.classList.remove("modal-content-show");
- }
-}
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode == 27) {
+    if (popup.classList.contains("modal-content-show")) {
+      popup.classList.remove("modal-content-show");
+    }
+  }
 });
